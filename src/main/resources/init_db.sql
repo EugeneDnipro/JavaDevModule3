5a5f775cf117d82e300ab7e435002933bb0db4cc
EugeneDnipro <eugenednepr@gmail.com>
2023-02-07 15:55:19 +0200

    Added some CONSTRAINTS

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -5,3 +5,5 @@ CREATE TABLE worker(
 	level VARCHAR NOT NULL,
 	salary INTEGER
 );
+ALTER TABLE worker ADD CONSTRAINT check_worker_name CHECK(LENGTH(name) >= 2 AND LENGTH(name) <= 1000);
+ALTER TABLE worker ADD CONSTRAINT check_worker_birthday CHECK(year (birthday) > 1900 );

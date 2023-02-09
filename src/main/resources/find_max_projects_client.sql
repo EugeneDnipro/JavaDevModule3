3366b9138598dae20386fa1f09ac718677074cdf
EugeneDnipro <eugenednepr@gmail.com>
2023-02-09 13:26:57 +0200

    find_max_projects_client.sql DONE

--- src/main/resources/find_max_projects_client.sql
+++ src/main/resources/find_max_projects_client.sql
@@ -1,1 +1,26 @@
-SELECT name, salary FROM client WHERE (salary = SELECT MAX(salary) FROM worker)
+--SELECT name, project.client_id
+--FROM client
+--JOIN project ON client.id = project.client_id;
+--
+--SELECT client_id, count(id) FROM project GROUP BY client_id;
+--
+--SELECT name, client_id
+--FROM (SELECT client_id, count(id) FROM project GROUP BY client_id)
+--JOIN client ON client.id = client_id;
+--
+--SELECT name, count(name) FROM (SELECT name, project.client_id
+--FROM client
+--JOIN project ON client.id = project.client_id) GROUP BY name;
+--
+--SELECT name, count(name) AS project_count
+--    FROM (SELECT name, project.client_id
+--        FROM client
+--        JOIN project ON client.id = project.client_id)
+--    GROUP BY name
+
+SELECT name, count(name) AS project_count
+    FROM (SELECT name, project.client_id
+        FROM client
+        JOIN project ON client.id = project.client_id)
+    GROUP BY name
+    HAVING project_count = SELECT (MAX (project_count))

5b471c49f068f93bb5fb1c92e06c2bd59e946b26
EugeneDnipro <eugenednepr@gmail.com>
2023-02-16 13:14:34 +0200

    Changes in find_youngest_eldest_workers.sql

--- src/main/resources/find_youngest_eldest_workers.sql
+++ src/main/resources/find_youngest_eldest_workers.sql
@@ -1,19 +1,7 @@
-CREATE TABLE youngest_eldest_workers(
-	type VARCHAR PRIMARY KEY,
-	name VARCHAR,
-	birthday DATE,
-);
-
-ALTER TABLE youngest_eldest_workers ADD CONSTRAINT check_youngest_eldest_workers_type CHECK(type IN ('YOUNGEST', 'ELDEST'));
-
-INSERT INTO youngest_eldest_workers (type, name, birthday)
-    VALUES ('YOUNGEST',
-        (SELECT name FROM worker WHERE birthday=(SELECT MAX(birthday) FROM worker)),
-        (SELECT birthday FROM worker WHERE birthday=(SELECT MAX(birthday) FROM worker)));
-
-INSERT INTO youngest_eldest_workers (type, name, birthday)
-    VALUES ('ELDEST',
-        (SELECT name FROM worker WHERE birthday=(SELECT MIN(birthday) FROM worker)),
-        (SELECT birthday FROM worker WHERE birthday=(SELECT MIN(birthday) FROM worker)));
-
-SELECT * FROM youngest_eldest_workers;
+SELECT
+CASE WHEN birthday=(SELECT MAX(birthday) FROM worker) THEN 'YOUNGEST'
+     WHEN birthday=(SELECT MIN(birthday) FROM worker) THEN 'ELDEST'
+END AS type, name, birthday
+FROM worker WHERE (CASE WHEN birthday=(SELECT MAX(birthday) FROM worker) THEN 'YOUNGEST'
+                        WHEN birthday=(SELECT MIN(birthday) FROM worker) THEN 'ELDEST'
+                   END) != 'null';
